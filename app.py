7196f4f278abbf4bbb675a73e85f90ea7b5b13e7
Rwan Mahmoud <rwanmahmoud@Rwans-MacBook-Pro.local>
2020-01-06 18:02:34 +0200

    initial commit

--- app.py
+++ app.py
@@ -79,5 +79,6 @@ def next_ticker():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=33507)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port)
